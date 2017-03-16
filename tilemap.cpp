f2e25e0a710287577c14986e93577dffd42381b1
Ryan Goldstein <ryanisaacg@gmail.com>
2017-03-15 20:02:49 -0400

    Fixed various tilemap behavior

--- tilemap.cpp
+++ tilemap.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <iostream>
 
 #include "tilemap.hpp"
 
@@ -7,9 +8,9 @@ Tilemap::Tilemap(int width, int height, int tile_width, int tile_height)
 	std::fill(buffer, buffer + width * height, 0);
 }
 int Tilemap::get_index(float x, float y) {
-	int xSpot = (int)(x * tile_width / width);
-	int ySpot = (int)(y * tile_height / height);
-	return buffer[xSpot * height + ySpot];
+	int xSpot = (int)(x / tile_width);
+	int ySpot = (int)(y / tile_height);
+	return xSpot * tile_height + ySpot;
 }
 
 void Tilemap::set(int data, float x, float y) {
@@ -30,16 +31,17 @@ bool Tilemap::free(float x, float y) {
 }
 
 bool Tilemap::free(float x, float y, float width, float height) {
-	int lowestX = (int)(x * tile_width / this->width);
-	int lowestY = (int)(y * tile_height / this->height);
-	int highestX = (int)((x + width) * tile_height / this->width + 0.5f);
-	int highestY = (int)((y + height) * tile_height / this->height + 0.5f);
+	int lowestX = (int)(x / tile_width);
+	int lowestY = (int)(y / tile_height);
+	int highestX = (int)((x + width) / tile_height + 0.5f);
+	int highestY = (int)((y + height) / tile_height + 0.5f);
+	std::cout << (highestY - lowestY) << std::endl;
 	if(!valid(x, y) || !valid(x + width, y + height)) {
 		return false;
 	}
-	for(int i = lowestX; i < highestX; i++) 
-		for(int j = lowestY; j < highestY; j++) 
-			if(buffer[i * this->height + j])
+	for(int i = lowestX; i <= highestX; i++) 
+		for(int j = lowestY; j <= highestY; j++) 
+			if(buffer[i * this->tile_height + j])
 				return false;
 	return true;
 }
