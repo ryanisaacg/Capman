a36a61d0b97e6ea1390996f2a6d0cff9f6db1eee
Ryan Goldstein <ryanisaacg@gmail.com>
2017-03-18 21:38:13 -0400

    Added a very basic editor

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 #include "render.hpp"
 #include "tilemap.hpp"
 
+sf::Text scoreDisplay;	
+int score = 0, health = 3, hurt_cooldown = 0, max_hurt_cooldown = 120;
 
 void load_level(std::string filename, Object &playerOut, std::vector<Object> &enemiesOut, Tilemap &mapOut) {
 	std::ifstream input(filename);
@@ -48,80 +50,121 @@ void spawn_pellets(Tilemap map, std::vector<Object> &pellets) {
 				pellets.push_back(Object(sf::Color::White, i + 16, j + 16, 6, 0.5f));
 }
 
+static void edit(sf::Window &window, Tilemap map, Object &player, std::vector<Object> &enemies, bool justPressedE) {
+	auto mouse_pos = sf::Mouse::getPosition(window);
+	if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+		map.set(true, mouse_pos.x, mouse_pos.y);
+	} else if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Right)) {
+		map.set(false, mouse_pos.x, mouse_pos.y);
+		for(auto enemy = enemies.begin(); enemy < enemies.end(); enemy++)
+			if(enemy->contains(mouse_pos.x, mouse_pos.y)) 
+				enemy = enemies.erase(enemy);
+	}
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::P)) {
+		player.x = mouse_pos.x;
+		player.y = mouse_pos.y;
+	}
+	if(justPressedE)
+		enemies.push_back(Object(sf::Color::Red, mouse_pos.x, mouse_pos.y, 10));
+}
+
+static void update(Tilemap map, Object &player, std::vector<Object> &pellets, std::vector<Object> &ghostPellets, std::vector<Object> &enemies) {
+	//Walk
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		player.xspeed = -4;
+	} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+		player.xspeed = 4;
+	} else {
+		player.xspeed = 0;
+	}
+	player.fall(map, 0.5);
+	//Jump
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player.supported(map))
+		player.yspeed = -12;
+	//Update all of the pellets
+	for(auto pellet = pellets.begin(); pellet < pellets.end(); pellet++) {
+		if(pellet->collides(player)) {
+			ghostPellets.push_back(Object(sf::Color::Yellow, player.x, player.y, pellet->radius * 2));
+			pellet = pellets.erase(pellet);
+			score++;
+			scoreDisplay.setString(std::to_string(score));
+			scoreDisplay.setPosition(window_width / 2 - scoreDisplay.getLocalBounds().width / 2, 0);
+		}
+	}
+	//Update the ghost pellet effect
+	for(auto ghost = ghostPellets.begin(); ghost < ghostPellets.end(); ghost++) {
+		if(ghost->radius <= 0) {
+			ghost = ghostPellets.erase(ghost);
+		} else {
+			ghost->radius -= 0.5;
+			ghost->color.a -= 10;
+		}
+	}
+	//Move the enemies towards the player
+	for(auto &enemy : enemies) {
+		if(abs(enemy.x - player.x) < 2) {
+			enemy.x = player.x;
+			enemy.xspeed = 0;
+		} else if(enemy.x < player.x) {
+			enemy.xspeed = 2;
+		} else if(enemy.x > player.x) {
+			enemy.xspeed = -2;
+		}
+		if(enemy.yspeed < 8 && enemy.y > player.y) {
+			enemy.yspeed = -4;
+		}
+		enemy.fall(map, 0.25);
+		if(hurt_cooldown == 0 && enemy.collides(player)) {
+			hurt_cooldown = max_hurt_cooldown;
+			health -= 1;
+		}
+	}
+	if(hurt_cooldown > 0) hurt_cooldown--;
+}
+
 int main() {
 	Tilemap map(game_width, game_height, 32, 32);
-    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "SFML works!");
 	Object player(sf::Color::Cyan, 0, 0, 10);
-	sf::Clock clock;
 	std::vector<Object> pellets, ghostPellets, enemies;
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "SFML works!");
+	window.setKeyRepeatEnabled(false);
+	sf::Clock clock;
 	sf::Font font;
 	font.loadFromFile("arial.ttf");
-	sf::Text scoreDisplay("", font);
+	scoreDisplay = sf::Text("", font);
 	scoreDisplay.setOutlineColor(sf::Color::Yellow);
 	scoreDisplay.setFillColor(sf::Color::White);
 	scoreDisplay.setOutlineThickness(0.5);
-	int score = 0, health = 3, hurt_cooldown = 0, max_hurt_cooldown = 120;
+	bool editing = false;
+	bool previous_switch = false;
 
 	load_level("level1", player, enemies, map);
 	spawn_pellets(map, pellets);
 
     while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {
+		bool justPressedE = false;
+        while (window.pollEvent(event)) {	
             if (event.type == sf::Event::Closed)
                 window.close();
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Key::E)
+				justPressedE = true;
         }
-		//Walk
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-			player.xspeed = -4;
-		} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-			player.xspeed = 4;
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::PageUp)) {
+			if(!previous_switch) { 
+				editing = !editing;
+				previous_switch = true;
+			}
 		} else {
-			player.xspeed = 0;
+			previous_switch = false;
 		}
-		player.fall(map, 0.5);
-		//Jump
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player.supported(map))
-			player.yspeed = -12;
-		//Update all of the pellets
-		for(auto pellet = pellets.begin(); pellet < pellets.end(); pellet++) {
-			if(pellet->collides(player)) {
-				ghostPellets.push_back(Object(sf::Color::Yellow, player.x, player.y, pellet->radius * 2));
-				pellet = pellets.erase(pellet);
-				score++;
-				scoreDisplay.setString(std::to_string(score));
-				scoreDisplay.setPosition(window_width / 2 - scoreDisplay.getLocalBounds().width / 2, 0);
-			}
+		
+		if(editing) {
+			edit(window, map, player, enemies, justPressedE); 
+		} else {
+			//Update the game
+			update(map, player, pellets, ghostPellets, enemies) ;
 		}
-		//Update the ghost pellet effect
-		for(auto ghost = ghostPellets.begin(); ghost < ghostPellets.end(); ghost++) {
-			if(ghost->radius <= 0) {
-				ghost = ghostPellets.erase(ghost);
-			} else {
-				ghost->radius -= 0.5;
-				ghost->color.a -= 10;
-			}
-		}
-		//Move the enemies towards the player
-		for(auto &enemy : enemies) {
-			if(abs(enemy.x - player.x) < 2) {
-				enemy.x = player.x;
-				enemy.xspeed = 0;
-			} else if(enemy.x < player.x) {
-				enemy.xspeed = 2;
-			} else if(enemy.x > player.x) {
-				enemy.xspeed = -2;
-			}
-			if(enemy.yspeed < 8 && enemy.y > player.y) {
-				enemy.yspeed = -4;
-			}
-			enemy.fall(map, 0.25);
-			if(hurt_cooldown == 0 && enemy.collides(player)) {
-				hurt_cooldown = max_hurt_cooldown;
-				health -= 1;
-			}
-		}
-		if(hurt_cooldown > 0) hurt_cooldown--;
 		//Render the game
 		render_state(window, map, player, enemies, pellets, ghostPellets, scoreDisplay, health);
 		//Handle frame timing
