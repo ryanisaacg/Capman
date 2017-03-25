4ea02062faf390514ab4fcb8950af3d705b4cfa1
Ryan Goldstein <ryanisaacg@gmail.com>
2017-03-25 16:59:19 -0400

    Added PNGs to the exported windows build

--- winbuild.sh
+++ winbuild.sh
@@ -2,5 +2,5 @@
 make clean
 make EXTRA-CFLAGS=-O3 EXTRA-LFLAGS=-Wl,-subsystem,windows-Wl,-subsystem,windows
 cp main.out main.exe
-7z a -tzip winbuild.zip main.exe *.ttf *.wav *.ogg tracks levels *.lvl *.dll
+7z a -tzip winbuild.zip main.exe *.ttf *.wav *.ogg tracks levels *.lvl *.dll *.png
 rm main.exe
